8325aa26c5e18e425fc6d456f2bde20ece21cbbc
Chaogebruce <romancavalrycc@gmail.coom>
2015-12-25 11:28:09 +0800

    add apis,coro

--- www/coroweb.py
+++ www/coroweb.py
@@ -14,13 +14,13 @@ def get(path):
     """
 
     def decorator(func):
-        @functools.warps(func)
-        def warpper(*args, **kw):
+        @functools.wraps(func)
+        def wrapper(*args, **kw):
             return func(*args, **kw)
 
-        warpper.__method__ = 'GET'
-        warpper.__router__ = path
-        return warpper
+        wrapper.__method__ = 'GET'
+        wrapper.__router__ = path
+        return wrapper
 
     return decorator
 
@@ -32,12 +32,12 @@ def post(path):
 
     def decorator(func):
         @functools.wraps(func)
-        def warpper(*args, **kw):
+        def wrapper(*args, **kw):
             return func(*args, **kw)
 
-        warpper.__method__ = 'POST'
-        warpper.__router__ = path
-        return warpper
+        wrapper.__method__ = 'POST'
+        wrapper.__router__ = path
+        return wrapper
 
     return decorator
 
@@ -83,7 +83,7 @@ def has_request_args(fn):
             found = True
             continue
         if found and (
-                    param.kind != inspect.Parameter.VAR_POSITIONAL and param.kind != inspect.Parameter.VAR_KEYWORD and param.kind != inspect.Parameter.KEYWORD_ONLY):
+                            param.kind != inspect.Parameter.VAR_POSITIONAL and param.kind != inspect.Parameter.VAR_KEYWORD and param.kind != inspect.Parameter.KEYWORD_ONLY):
             raise ValueError(
                 'request parameter must be  last named parameter in function: %s%s' % (fn.__name__, str(sig)))
     return found
@@ -105,7 +105,72 @@ class RequestHandler(object):
         if self._has_var_kw_args or self._required_kw_args or self._has_named_kw_args:
             if request.method == 'POST':
                 if not request.connect_type:
-                    return w
+                    return web.HTTPBadRequest('Missing Connect-Type.')
+                ct = request.connect_type.lower()
+                if ct.startswith('application/json'):
+                    params = yield from request.json()
+                    if not isinstance(params, dict):
+                        return web.HTTPBadRequest('JSON body must be object.')
+                    kw = params
+                elif ct.startswith('application/x-www-form-urlencoded') or ct.startswith('multipart/form-data'):
+                    params = yield from request.post()
+                    kw = dict(**params)
+                else:
+                    return web.HTTPBadRequest('Unsupported Connect-Type: %s' % request.connect_type)
+            if request.method == 'GEt':
+                qs = request.query_string
+                if qs:
+                    kw = dict()
+                    for k, v in parse.parse_qs(qs, True).items():
+                        kw[k] = v[0]
+        if kw is None:
+            kw = dict(**request.match_info)
+        else:
+            if not self._has_var_kw_args and self._named_kw_args:
+                copy = dict()
+                for name in self._named_kw_args:
+                    if not name in kw:
+                        return web.HTTPBadRequest('Missing argument; %s' % name)
+            logging.info('call with args: %s' % str(kw))
+            try:
+                r = yield from self._func(**kw)
+                return r
+            except APIError as e:
+                return dict(error=e.error, data=e, message=e.message)
+
+
+def add_static(app):
+    path = os.path.join(os.path.dirname(os.path.abspath(__file__)),'static')
+    app.router.add_static('/static/',path)
+    logging.info('add statoc %s => %s' % ('static',path))
+
+def add_route(app, fn):
+    method = getattr(fn, '__method__', None)
+    path = getattr(fn, '__route__', None)
+    if path is None or method is None:
+        raise ValueError('@get or @post not defined in %s.' % str(fn))
+    if not asyncio.iscoroutinefunction(fn) and not inspect.isgeneratorfunction(fn):
+        fn = asyncio.coroutine(fn)
+    logging.info('add route %s %s => %s(%s)' % (method, path, fn.__name__,  ','.join(inspect.signature(fn).parameters.keys())))
+    app.router.add_route(method, path, RequestHandler(app, fn))
+
+def add_routes(app, module_name):
+    n = module_name.rfind('.')
+    if n == (-1):
+        mod = __import__(module_name,globals(), locals())
+    else:
+        name = module_name[n+1:]
+        mod = getattr(__import__(module_name[:n], globals(), locals(),[name]), name)
+    for attr in dir(mod):
+        if attr.startswith('_'):
+            continue
+        fn = getattr(mod, attr)
+        if callable(fn):
+            method = getattr(fn, '__method__', None)
+            path = getattr(fn, '__route__', None)
+            if method and path:
+                add_route(app,fn)
+
 
 
 
