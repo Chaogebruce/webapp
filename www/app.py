fffedcaa0218e58c89fed0b5972da372145a7542
Chaogebruce <romancavalrycc@gmail.coom>
2015-12-25 16:13:06 +0800

    add config files

--- www/app.py
+++ www/app.py
@@ -13,22 +13,21 @@ logging.basicConfig(level=logging.INFO)
 
 import asyncio, os, json, time
 from datetime import datetime
-
 from aiohttp import web
 from jinja2 import Environment, FileSystemLoader
+import orm
+from coroweb import add_routes, add_static
 
-import www.orm
-from www.coroweb import add_routes, add_static
 
-def init_jinja2(app,  **kw):
+def init_jinja2(app, **kw):
     logging.info('init jinja2...')
     options = dict(
-        autoescape = kw.get('autoescape', True),
-        block_start_string = kw.get('block_start_string', '{%'),
-        block_end_string = kw.get('block_end_string','%}'),
-        variable_start_string =  kw.get('variable_start_string','{{'),
-        variable_end_string = kw.get('variable_end_string','}}'),
-        auto_reload = kw.get('auto_reload', True)
+        autoescape=kw.get('autoescape', True),
+        block_start_string=kw.get('block_start_string', '{%'),
+        block_end_string=kw.get('block_end_string', '%}'),
+        variable_start_string=kw.get('variable_start_string', '{{'),
+        variable_end_string=kw.get('variable_end_string', '}}'),
+        auto_reload=kw.get('auto_reload', True)
     )
     path = kw.get('path', None)
     if path is None:
@@ -41,6 +40,7 @@ def init_jinja2(app,  **kw):
             env.filters[name] = f
     app['__templates__'] = env
 
+
 @asyncio.coroutine
 def logger_factory(app, handler):
     @asyncio.coroutine
@@ -48,7 +48,9 @@ def logger_factory(app, handler):
         logging.info('Request: %s %s' % (request.method, request.path))
         # yield from asynico.sleep(0.3)
         return (yield from handler(request))
+
     return logger
+
 
 @asyncio.coroutine
 def data_factory(app, handler):
@@ -62,7 +64,9 @@ def data_factory(app, handler):
                 request.__data__ = yield request.post()
                 logging.info('request form: %s' % str(request.__data__))
         return (yield from handler(request))
+
     return parse_data
+
 
 @asyncio.coroutine
 def response_factory(app, handler):
@@ -70,7 +74,7 @@ def response_factory(app, handler):
     def response(request):
         logging.info('Request handler...')
         r = yield from handler(request)
-        if isinstance(r,web.StreamResponse):
+        if isinstance(r, web.StreamResponse):
             return r
         if isinstance(r, bytes):
             resp = web.Response(body=r)
@@ -82,20 +86,22 @@ def response_factory(app, handler):
             resp = web.Response(body=r.encode('utf-8'))
             resp.content_type = 'text/html;charset=utf-8'
             return resp
-        if isinstance(r,int) and t >= 100 and t <=600:
+        if isinstance(r, int) and t >= 100 and t <= 600:
             return web.Response(t)
-        if isinstance(r,tuple) and len(r) == 2:
+        if isinstance(r, tuple) and len(r) == 2:
             t, m = r
-            if isinstance(t, int) and t>= 100 and t < 600:
+            if isinstance(t, int) and t >= 100 and t < 600:
                 return web.Response(t, str(m))
-        #default
+        # default
         resp = web.Response(body=str(r).encode('utf-8'))
         resp.content_type = 'text/plain;charset=utf-8'
         return resp
+
     return response
 
+
 def datetime_filter(t):
-    delta = int(time.time()-t)
+    delta = int(time.time() - t)
     if delta < 60:
         return u'1分钟前'
     if delta < 3600:
@@ -106,6 +112,7 @@ def datetime_filter(t):
         return u'%s天前' % (delta // 86400)
     dt = datetime.fromtimestamp(t)
     return u'%s年%s月%s日' % (dt.year, dt.month, dt.day)
+
 
 @asyncio.coroutine
 def init(loop):
@@ -122,4 +129,3 @@ def init(loop):
 loop = asyncio.get_event_loop()
 loop.run_until_complete(init(loop))
 loop.run_forever()
-
--- www/handlers.py
+++ www/handlers.py
@@ -3,3 +3,16 @@
 
 __author__ = 'Bruce Chen'
 
+import re, time, json, logging, hashlib, base64, asyncio
+
+from coroweb import get,  post
+
+from models import User, Comment, Blog, next_id
+
+@get('/')
+def index(request):
+    users = yield from User.findAll()
+    return {
+        '__templates__': 'test.html',
+        'users':users
+    }
