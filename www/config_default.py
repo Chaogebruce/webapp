7832a027d718eba9e0b78435c4a6f3d6b5be3c0c
Chaogebruce <romancavalrycc@gmail.coom>
2015-12-25 15:55:33 +0800

    add config files

---
+++ www/config_default.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''
+Default configurations.
+'''
+__author__ = 'Bruce Chen'
+
+configs = {
+    'db': {
+        'host': '127.0.0.1',
+        'port': '3036',
+        'user': 'www-data',
+        'password': 'www-data',
+        'database': 'awesome'
+    },
+    'session': {
+        'secret': 'AwEsOme'
+    }
+}
---
+++ www/config_override.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''
+Override configuration
+'''
+__author__ = 'Bruce Chen'
+
+configs = {
+    'db': {
+        'host': '127.0.0.1'
+    }
+}
